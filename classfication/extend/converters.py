3ae537cb0a27aa2818c81c74a89b1ab93ab12717
mynameiseuiseok <sunges1014@naver.com>
2023-04-27 18:19:34 +0900

    airplane, calculator

--- classfication/extend/converters.py
+++ classfication/extend/converters.py
@@ -13,7 +13,6 @@ class Converter(ScaleConverter):
         # return self.factor * value + self.offset
         return super().convert(value) + self.offset
 
-
 conv = Converter('C', 'F', 1.8, 32)
 print("Converting 21C")
 print(f'{conv.convert(21):.2f}{conv.units_to}')
