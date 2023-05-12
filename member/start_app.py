4d614fe303c56bdd95b038f85bc12315e7b9b0ab
mynameiseuiseok <sunges1014@naver.com>
2023-05-12 18:20:33 +0900

    gangnam board

--- member/start_app.py
+++ member/start_app.py
@@ -1,8 +1,8 @@
 # 컨트롤러 start_app.py
 # templates 폴더, static 폴더
 # 웹 서버 - flask
-
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, \
+    redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def getconn():
 print(getconn())
 
 # url - '/' 경로 설정
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
 
@@ -29,7 +29,7 @@ def memberlist():
     cursor.execute(sql)  # 검색 수행
     rs = cursor.fetchall()
     conn.close()
-
+    # db에 저장된 자료(rs)를 memberlist.html에 보내줌
     return render_template('memberlist.html', rs=rs)
 
 # 회원 가입
@@ -42,7 +42,7 @@ def register():
         name = request.form['name']
         gender = request.form['gender']
 
-        # db에 저장
+        #db에 저장
         conn = getconn()
         cursor = conn.cursor()
         sql = f"INSERT INTO member(memberid, passwd, name, gender) " \
@@ -50,22 +50,22 @@ def register():
         # 자동 로그인 - 세션 발급
         session['userid'] = request.form['memberid']
         cursor.execute(sql)  # 검색 수행
-        conn.commit()   # 커밋 완료
+        conn.commit()  # 커밋 완료
         conn.close()
-        # redirect - 회원 가입 후 회원 목록 페이지로 강제로 이동 시킴
+        # redirect - 회원가입후 회원 목록 페이지로 강제로 이동 시킴
         return redirect(url_for('memberlist'))
     else:
         return render_template('register.html')
 
-# 로그인 페이지
+#로그인 페이지
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == "POST":
-        # 로그인 폼에 입력된 데이터 넘겨 받음
+        # 로그인폼에 입력된 데이터 넘겨 받음
         id = request.form['memberid']
         pw = request.form['passwd']
 
-        # database에 접속 - 로그인 체크
+        #database에 접속 - 로그인 체크
         conn = getconn()
         cursor = conn.cursor()
         # 동적 바인딩
@@ -77,7 +77,7 @@ def login():
         conn.close()
 
         if rs: # rs = True
-            session['userid'] = rs[0]   # memberid를 세션에 저장(세션 발급)
+            session['userid'] = rs[0]  #memberid를 세션에 저장(세션 발급)
             return redirect(url_for('index'))
         else:
             error_msg = "아이디나 비밀번호를 확인해주세요"
@@ -88,7 +88,7 @@ def login():
 # 로그 아웃
 @app.route('/logout', methods = ['GET'])
 def logout():
-    session.clear()   # 모든 세션 삭제
+    session.clear()  #모든 세션 삭제
     return redirect(url_for('index'))
 
 # 게시판 목록
@@ -108,15 +108,15 @@ def boardlist():
 @app.route('/writing', methods=['GET', 'POST'])
 def writing():
     if request.method == 'POST':
-        # 입력된 제목과 글, 내용을 가져와서 DB에 저장
-        title = request.form['title']
-        content = request.form['content']
+        # 입력된 제목,글내용을 가져와서 DB에 저장
+        title = request.form['title'].replace("'", "''")
+        content = request.form['content'].replace("'", "''")
         # userid : session 이름을 가져옴
         memberid = session.get('userid')
 
         conn = getconn()
         cursor = conn.cursor()
-        sql = f"INSERT INTO board(title, content, memberid) "\
+        sql = f"INSERT INTO board(title, content, memberid) " \
               f"VALUES ('{title}', '{content}', '{memberid}')"
         cursor.execute(sql)
         conn.commit()
@@ -127,13 +127,20 @@ def writing():
 
 # 글 상세보기
 @app.route('/detail/<int:bno>', methods=['GET'])
-def detail(bno): # 매개변수로 bno 설정
+def detail(bno):  #매개변수로 bno 설정
     # DB의 board 테이블에서 bno로 검색된 글 가져오기
     conn = getconn()
     cursor = conn.cursor()
     sql = f"SELECT * FROM board WHERE bno = {bno}"
     cursor.execute(sql)
-    board = cursor.fetchone()   # 게시글 1개 가져옴
+    board = cursor.fetchone()  # 게시글 1개 가져옴
+
+    # 조회수 증가
+    hit = board[4]
+    sql = f"UPDATE board SET hit = {hit + 1} WHERE bno = {bno}"
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
     return render_template('detail.html', board=board)
 
 # 게시글 삭제
@@ -142,10 +149,37 @@ def delete(bno):
     # 삭제 요청한 글 번호를 DB board 테이블 삭제
     conn = getconn()
     cursor = conn.cursor()
-    sql = f"DELETE FROM board WHERE bno = {bno};"   # 숫자이므로 따옴표 붙이지 않음
+    sql = f"DELETE FROM board WHERE bno = {bno}"  #숫자이므로 따옴표 붙이지 않음
     cursor.execute(sql)
     conn.commit()
     conn.close()
     return redirect(url_for('boardlist'))
+
+# 게시글 수정
+@app.route('/update/<int:bno>', methods=['GET', 'POST'])
+def update(bno):
+    if request.method == "POST":
+        # 수정한 입력 내용을 board 테이블에 저장
+        title = request.form['title'].replace("'","''")
+        content = request.form['content'].replace("'","''")
+
+        # DB에 저장
+        conn = getconn()
+        cursor = conn.cursor()
+        sql = f"UPDATE board SET title = '{title}', content = '{content}' " \
+              f"WHERE bno = {bno}"
+        cursor.execute(sql)
+        conn.commit()
+        conn.close()
+        return redirect(url_for('detail', bno=bno)) #상세보기(글번호 명시)
+    else:
+        # 수정할 글(board)을 db에 가져오기
+        conn = getconn()
+        cursor = conn.cursor()
+        sql = f"SELECT * FROM board WHERE bno = {bno}"
+        cursor.execute(sql)
+        board = cursor.fetchone()  #게시글 1개 반환받음
+        conn.close()
+        return render_template('update.html', board=board)
 
 app.run()
