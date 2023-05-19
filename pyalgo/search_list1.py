e013552d0051a321088deed885fdd3a5993e162b
mynameiseuiseok <sunges1014@naver.com>
2023-05-19 18:17:59 +0900

    binary, game

--- pyalgo/search_list1.py
+++ pyalgo/search_list1.py
@@ -2,11 +2,11 @@
 # 리스트의 첫번째 자료부터 하나씩 비교하면서 같은 값이 나오면
 # 그 위치를 돌려주고(반환), 못 찾으면 -1을 반환함
 
-def search_list(a, x):
+def search_list(a, x):  # 리스트, 찾는 값
     n = len(v)
-    for i in range(0, n):
-        if a[i] == x:
-            return i
+    for i in range(0, n):   # 모든 값을 차례로 반복
+        if a[i] == x:   # 리스트의 값이 찾는 값과 비교하여 같으면
+            return i    # 위치를 반환함
     return -1
 
 def search_list2(a, x):
@@ -15,12 +15,16 @@ def search_list2(a, x):
     for i in range(0, n):
         if a[i] == x:
             same_num.append(i)
+
     # 리스트에 저장된 값이 없으면
     if len(same_num) == 0:
         # return -1
         return "값을 찾을 수 없습니다."
     else:
         return same_num
+
+def search_list3():
+    pass
 
 v = [60, 5, 33, 5, 12, 97, 24]
 
